1b24c0ba7d1457c5db6f29f6cfaa83a79ab9f607
Viacheslav Plevako <cheryec@ukr.net>
2024-04-05 13:42:20 +0300

    add metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,34 @@ import { Footer } from "@/components/Footer/Footer";
 import { biroScript, messinaSans, rightGrotesk } from "@/fonts/fonts";
 import PropTypes from "prop-types";
 
+export const metadata = {
+  title: "APE NFT",
+  description:
+    "Yacht ape is a collectionof unique digital apes that you can own in nft format",
+  openGraph: {
+    title: "APE NFT",
+    description:
+      "Yacht ape is a collectionof unique digital apes that you can own in nft format",
+    url: "https://ape-nft-three.vercel.app/",
+    siteName: "APE NFT",
+    images: [
+      {
+        url: "https://ape-nft-three.vercel.app/Apes.png", // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://ape-nft-three.vercel.app/Apes.png", // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: "Come to our site",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
